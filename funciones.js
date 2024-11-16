f3807828622684e672afc7d11326beb3ce42fb01
JuanLehue <juancholehue@gmail.com>
2024-11-16 16:36:20 -0300

    cambios para reentrega, ajuste de funciones

--- funciones.js
+++ funciones.js
@@ -1,20 +1,67 @@
-function cargarTexto(texto, index, x, y, anT, an, al, align, tamaño) {
+function cargarBloque(img, texto, index, x, y, an, al, tamF, align, anBtn, alBtn, segundoBoton, tb1, tb2) {
   push();
+  imageMode(align);
+  image(img[index], 0, 0);
   fill(0, 200);
   stroke(255);
   rect(x, y, an, al);
   pop();
   push();
   fill(255);
-  textSize(tamaño);
+  textSize(tamF);
   textAlign(align);
-  text(texto[index], x+5, y+5, anT);
+  text(texto[index], x+5, y+5, an);
+  pop();
+  boton(x+an-anBtn-10, y+al-alBtn-10, anBtn, alBtn, tb1);
+  if (segundoBoton) {
+    boton(x+an-anBtn*2-20, y+al-alBtn-10, anBtn, alBtn, tb2);
+  }
+}
+
+function cargarInicio(img, texto, index, x, y, an, al, align, fuente, anBtn, alBtn) {
+  push();
+  imageMode(align);
+  image(img[index], x, y);
+  fill(0, 200);
+  rectMode(align);
+  rect(x, y, an, al);
+  fill(240);
+  textFont(fuente);
+  stroke(60, 40, 20);
+  strokeWeight(5);
+  textSize(32);
+  textAlign(align);
+  text(texto[index], x, y - al/1.5);
+  pop();
+  push();
+  boton(x - anBtn/2, y - alBtn*2, anBtn, alBtn, "Empezar");
+  boton(x - anBtn/2, y + alBtn, anBtn, alBtn, "Creditos");
   pop();
 }
 
-function cargarImagen(img, index, x, y, align) {
+function cargarCreditos(img, texto, index, x, y, an, al, align, fuente, anBtn, alBtn) {
+  push();
   imageMode(align);
   image(img[index], x, y);
+  fill(0, 200);
+  rectMode(align);
+  rect(x, y, an, al);
+  fill(255);
+  textFont(fuente);
+  stroke(60, 40, 20);
+  strokeWeight(5);
+  textAlign(align);
+  textSize(32);
+  text(texto[index], x, y - al/1.5);
+  pop();
+  push();
+  fill(255);
+  textSize(20);
+  textAlign(align);
+  text("Hecho por:\nJuan Lehue\nAlex Palomeque", x, y-al/3);
+  text("Autor del cuento:\nJulio Cortazar", x, y + al/5);
+  pop();
+  boton(x - anBtn/2, y + alBtn*4, anBtn, alBtn, "Volver al inicio");
 }
 
 
--- tpfinalparte1.js
+++ tpfinalparte1.js
@@ -3,6 +3,11 @@
 //Continuidad de los parques
 //https://www.youtube.com/watch?v=17726ni4dpc&ab_channel=JuanLehue
 
+/*
+ |¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯|
+ | FALTA ACOMODAR LOS MOUSEPRESSED DE LOS BOTONES DEL INICIO Y DE LOS CREDITOS!!!!!!!!!!!|
+ |_______________________________________________________________________________________|
+ */
 let textos = [];
 let imagenes = [];
 let bloque, mitadAn, mitadAl;
@@ -22,9 +27,7 @@ function preload() {
 function setup() {
   createCanvas(640, 480);
   bloque = "inicio";
-  mitadAn = width/2;
-  mitadAl = height/2;
-  for (let i=1; i<16; i++) {
+  for (let i=0; i<17; i++) {
     imagenes[i].resize(640, 640);
   }
   ambiente.amp(0.2);
@@ -35,132 +38,71 @@ function setup() {
 function draw() {
   background(225);
   if (bloque === "inicio") {
-    cargarImagen(imagenes, 0, mitadAn, mitadAl, CENTER);
-    push();
-    textFont(titulo);
-    stroke(60, 40, 20);
-    strokeWeight(5);
-    cargarTexto(textos, 0, 100, 80, 380, 0, 0, LEFT, 30);
-    pop();
-    fill(0, 200);
-    rect(172, 155, 200, 200);
-    boton(220, 200, 100, 30, "Empezar");
-    boton(220, 280, 100, 30, "Creditos");
+    cargarInicio(imagenes, textos, 0, width/2, height/2, 200, 200, CENTER, titulo, 100, 30);
   } else if (bloque === "intro") {
-    cargarImagen(imagenes, 1, mitadAn, mitadAl, CENTER);
-    cargarTexto(textos, 1, 50, 330, 500, 500, 130, LEFT, 16);
-    boton(270, 420, 130, 30, "Seguir leyendo");
-    boton(420, 420, 100, 30, "Levantarse");
+    cargarBloque(imagenes, textos, 1, 50, 330, 500, 130, 16, LEFT, 140, 30, true, "Levantarse", "Seguir leyendo");
   } else if (bloque === "ignorar") {
-    cargarImagen(imagenes, 2, mitadAn, mitadAl, CENTER);
-    cargarTexto(textos, 2, 50, 350, 500, 500, 115, LEFT, 16);
-    boton(400, 420, 130, 30, "Seguir leyendo");
+    cargarBloque(imagenes, textos, 2, 50, 350, 500, 110, 16, LEFT, 140, 30, false, "Seguir leyendo");
   } else if (bloque === "vertigo") {
-    cargarImagen(imagenes, 3, mitadAn, mitadAl, CENTER);
-    cargarTexto(textos, 3, 50, 330, 500, 500, 130, LEFT, 16);
-    boton(400, 420, 100, 30, "Continuar");
+    cargarBloque(imagenes, textos, 3, 50, 330, 500, 130, 16, LEFT, 140, 30, false, "Continuar");
   } else if (bloque === "final1") {
-    cargarImagen(imagenes, 4, mitadAn, mitadAl, CENTER);
-    cargarTexto(textos, 4, 50, 350, 500, 500, 110, LEFT, 16);
-    boton(400, 420, 140, 30, "Volver al inicio");
+    cargarBloque(imagenes, textos, 4, 50, 350, 500, 110, 16, LEFT, 140, 30, false, "Volver al inicio");
   } else if (bloque === "levantarse") {
-    cargarImagen(imagenes, 5, mitadAn, mitadAl, CENTER);
-    cargarTexto(textos, 5, 50, 350, 500, 500, 110, LEFT, 16);
-    boton(270, 420, 100, 30, "Volver");
-    boton(420, 420, 100, 30, "Investigar");
+    cargarBloque(imagenes, textos, 5, 50, 350, 500, 110, 16, LEFT, 140, 30, true, "Investigar", "Volver");
   } else if (bloque === "volver") {
-    cargarImagen(imagenes, 6, mitadAn, mitadAl, CENTER);
-    cargarTexto(textos, 6, 50, 350, 500, 500, 110, LEFT, 16);
-    boton(400, 420, 130, 30, "Seguir leyendo");
+    cargarBloque(imagenes, textos, 6, 50, 350, 500, 110, 16, LEFT, 140, 30, false, "Seguir leyendo");
   } else if (bloque === "salir") {
-    cargarImagen(imagenes, 7, mitadAn, mitadAl, CENTER);
-    cargarTexto(textos, 7, 50, 350, 500, 500, 110, LEFT, 16);
-    boton(400, 420, 100, 30, "Continuar");
+    cargarBloque(imagenes, textos, 7, 50, 350, 500, 110, 16, LEFT, 140, 30, false, "Continuar");
   } else if (bloque === "continuar") {
-    cargarImagen(imagenes, 8, mitadAn, mitadAl, CENTER);
-    cargarTexto(textos, 8, 50, 350, 500, 500, 110, LEFT, 16);
-    boton(270, 420, 100, 30, "Volver");
-    boton(400, 420, 130, 30, "Abrir la puerta");
+    cargarBloque(imagenes, textos, 8, 50, 350, 500, 110, 16, LEFT, 140, 30, true, "Abrir la puerta", "Volver");
   } else if (bloque === "empujar") {
-    cargarImagen(imagenes, 9, mitadAn, mitadAl, CENTER);
-    cargarTexto(textos, 9, 50, 350, 500, 500, 110, LEFT, 16);
-    boton(400, 420, 100, 30, "Enfrentarlo");
+    cargarBloque(imagenes, textos, 9, 50, 350, 500, 110, 16, LEFT, 140, 30, false, "Enfrentarlo");
   } else if (bloque === "enfrentamiento") {
-    cargarImagen(imagenes, 10, mitadAn, mitadAl, CENTER);
-    cargarTexto(textos, 10, 50, 350, 500, 500, 110, LEFT, 16);
-    boton(400, 420, 100, 30, "Continuar");
+    cargarBloque(imagenes, textos, 10, 50, 350, 500, 110, 16, LEFT, 140, 30, false, "Continuar");
   } else if (bloque === "final2") {
-    cargarImagen(imagenes, 11, mitadAn, mitadAl, CENTER);
-    cargarTexto(textos, 11, 50, 370, 500, 500, 90, LEFT, 16);
-    boton(400, 420, 130, 30, "Volver al inicio");
+    cargarBloque(imagenes, textos, 11, 50, 370, 500, 90, 16, LEFT, 140, 30, false, "Volver al inicio");
   } else if (bloque === "miedo") {
-    cargarImagen(imagenes, 12, mitadAn, mitadAl, CENTER);
-    cargarTexto(textos, 12, 50, 350, 500, 500, 110, LEFT, 16);
-    boton(260, 420, 130, 30, "Seguir leyendo");
-    boton(400, 420, 130, 30, "Cerrar el libro");
+    cargarBloque(imagenes, textos, 12, 50, 350, 500, 110, 16, LEFT, 140, 30, true, "Cerrar el libro", "Seguir leyendo");
   } else if (bloque === "atrapar") {
-    cargarImagen(imagenes, 13, mitadAn, mitadAl, CENTER);
-    cargarTexto(textos, 13, 50, 350, 500, 500, 110, LEFT, 16);
-    boton(400, 420, 100, 30, "Continuar");
+    cargarBloque(imagenes, textos, 13, 50, 350, 500, 110, 16, LEFT, 140, 30, false, "Continuar");
   } else if (bloque === "cerrar") {
-    cargarImagen(imagenes, 14, mitadAn, mitadAl, CENTER);
-    cargarTexto(textos, 14, 50, 350, 500, 500, 110, LEFT, 16);
-    boton(400, 420, 100, 30, "Continuar");
+    cargarBloque(imagenes, textos, 14, 50, 350, 500, 110, 16, LEFT, 140, 30, false, "Continuar");
   } else if (bloque === "final3") {
-    cargarImagen(imagenes, 15, mitadAn, mitadAl, CENTER);
-    cargarTexto(textos, 15, 50, 350, 500, 500, 110, LEFT, 16);
-    boton(400, 420, 130, 30, "Volver al inicio");
+    cargarBloque(imagenes, textos, 15, 50, 350, 500, 110, 16, LEFT, 140, 30, false, "Volver al inicio");
   } else if (bloque === "creditos") {
-    cargarImagen(imagenes, 16, mitadAn, mitadAl, CENTER);
-    push();
-    fill(0, 200);
-    rect(172, 155, 200, 200);
-    push();
-    textFont(titulo);
-    stroke(60, 40, 20);
-    strokeWeight(5);
-    cargarTexto(textos, 16, 210, 80, 380, 0, 0, LEFT, 30);
-    pop()
-      fill(255);
-    textSize(20);
-    text("Juan Lehue", 200, 200);
-    text("Alex Palomeque", 200, 230);
-    text("Autor del cuento:", 200, 280);
-    text("Julio Cortazar", 200, 310);
-    pop();
-    boton(200, 400, 140, 30, "Volver al inicio");
+    cargarCreditos(imagenes, textos, 16, width/2, height/2, 200, 200, CENTER, titulo, 140, 30);
   }
 }
 
 function mousePressed() {
   if (bloque === "inicio") {
-    if (detectarMouse(220, 200, 100, 30)) {
+    if (detectarMouse(270, 180, 100, 30)) {
       bloque="intro";
       if (!ambiente.isPlaying()) {
         ambiente.loop(true);
       }
       click.play();
     }
-    if (detectarMouse(220, 280, 100, 30)) {
+    if (detectarMouse(270, 270, 100, 30)) { 
       bloque="creditos";
       click.play();
     }
   } else if (bloque === "intro") {
-    if (detectarMouse(270, 420, 130, 30)) {
+    if (detectarMouse(270, 420, 140, 30)) {
       bloque="ignorar";
       click.play();
     }
-    if (detectarMouse(420, 420, 100, 30)) {
+    if (detectarMouse(400, 420, 140, 30)) {
       bloque="levantarse";
       click.play();
     }
   } else if (bloque === "ignorar") {
-    if (detectarMouse(400, 420, 130, 30)) {
+    if (detectarMouse(400, 420, 140, 30)) {
       bloque="vertigo";
       click.play();
     }
   } else if (bloque === "vertigo") {
-    if (detectarMouse(400, 420, 100, 30)) {
+    if (detectarMouse(400, 420, 140, 30)) {
       bloque="final1";
       click.play();
     }
@@ -172,78 +114,78 @@ function mousePressed() {
       click.play();
     }
   } else if (bloque === "levantarse") {
-    if (detectarMouse(270, 420, 100, 30)) {
+    if (detectarMouse(270, 420, 140, 30)) {
       bloque="volver";
       click.play();
     }
-    if (detectarMouse(420, 420, 100, 30)) {
+    if (detectarMouse(400, 420, 140, 30)) {
       bloque="salir";
       click.play();
     }
   } else if (bloque === "volver") {
-    if (detectarMouse(400, 420, 130, 30)) {
+    if (detectarMouse(400, 420, 140, 30)) {
       bloque="vertigo";
       click.play();
     }
   } else if (bloque === "salir") {
-    if (detectarMouse(400, 420, 100, 30)) {
+    if (detectarMouse(400, 420, 140, 30)) {
       bloque="continuar";
       click.play();
     }
   } else if (bloque === "continuar") {
-    if (detectarMouse(400, 420, 130, 30)) {
+    if (detectarMouse(400, 420, 140, 30)) {
       bloque="empujar";
       click.play();
     }
-    if (detectarMouse(270, 420, 100, 30)) {
+    if (detectarMouse(270, 420, 140, 30)) {
       bloque="miedo";
       click.play();
     }
   } else if (bloque === "empujar") {
-    if (detectarMouse(400, 420, 100, 30)) {
+    if (detectarMouse(400, 420, 140, 30)) {
       bloque="enfrentamiento";
       click.play();
     }
   } else if (bloque === "enfrentamiento") {
-    if (detectarMouse(400, 420, 100, 30)) {
+    if (detectarMouse(400, 420, 140, 30)) {
       bloque="final2";
       click.play();
     }
   } else if (bloque === "final2") {
-    if (detectarMouse(400, 420, 130, 30)) {
+    if (detectarMouse(400, 420, 140, 30)) {
       bloque="inicio";
       ambiente.loop(false);
       ambiente.stop();
       click.play();
     }
   } else if (bloque === "miedo") {
-    if (detectarMouse(260, 420, 130, 30)) {
+    if (detectarMouse(260, 420, 140, 30)) {
       bloque="atrapar";
       click.play();
     }
-    if (detectarMouse(400, 420, 130, 30)) {
+    if (detectarMouse(400, 420, 140, 30)) {
       bloque="cerrar";
       click.play();
     }
   } else if (bloque === "atrapar") {
-    if (detectarMouse(400, 420, 100, 30)) {
+    if (detectarMouse(400, 420, 140, 30)) {
       bloque="final1";
       click.play();
     }
   } else if (bloque === "cerrar") {
-    if (detectarMouse(400, 420, 100, 30)) {
+    if (detectarMouse(400, 420, 140, 30)) {
       bloque="final3";
       click.play();
     }
   } else if (bloque === "final3") {
-    if (detectarMouse(400, 420, 130, 30)) {
+    if (detectarMouse(400, 420, 140, 30)) {
       bloque="inicio";
       ambiente.loop(false);
       ambiente.stop();
       click.play();
     }
   } else if (bloque === "creditos") {
-    if (detectarMouse(220, 400, 140, 30)) {
+    if (detectarMouse(250, 360, 140, 30)) {
       bloque="inicio";
       click.play();
     }
